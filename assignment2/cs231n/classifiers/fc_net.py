1c3dbb083f644b95d35d179ada34f8126b813e9d
Warren <warren.kushner@gmail.com>
2017-01-08 17:45:38 -0500

    added logic to find loss in fullyconnected layer

--- assignment2/cs231n/classifiers/fc_net.py
+++ assignment2/cs231n/classifiers/fc_net.py
@@ -45,7 +45,14 @@ class TwoLayerNet(object):
     # weights and biases using the keys 'W1' and 'b1' and second layer weights #
     # and biases using the keys 'W2' and 'b2'.                                 #
     ############################################################################
-    pass
+    w1 = np.random.normal(loc=0.0, scale=weight_scale, size=(input_dim,hidden_dim))
+    self.params['W1'] = w1
+    self.params['b1'] = np.zeros(hidden_dim)
+
+    w2 = np.random.normal(loc=0.0, scale=weight_scale, size=(hidden_dim,num_classes))
+    self.params['W2'] = w1
+    self.params['b2'] = np.zeros(num_classes)
+    # self.params['b1'] = np.ones()
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -75,7 +82,10 @@ class TwoLayerNet(object):
     # TODO: Implement the forward pass for the two-layer net, computing the    #
     # class scores for X and storing them in the scores variable.              #
     ############################################################################
-    pass
+    L1, cache1 = affine_forward(X,self.params['W1'],self.params['b1'])
+    L2, cache2 = affine_forward(L1,self.params['W2'],self.params['b2'])
+
+    scores = L2
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -95,7 +105,22 @@ class TwoLayerNet(object):
     # automated tests, make sure that your L2 regularization includes a factor #
     # of 0.5 to simplify the expression for the gradient.                      #
     ############################################################################
-    pass
+
+    loss, dloss = softmax_loss(L2,y)  
+    reg_loss = 0.5*self.reg*(np.sum(self.params['W2']*self.params['W2'])+np.sum(self.params['W1']*self.params['W1']))
+    loss += reg_loss
+    dL2, dW2, db2 = affine_backward(dloss,cache2)
+    
+    dW2+=self.reg*self.params['W2']
+
+    grads['W2'] = dW2
+    grads['b2'] = db2
+
+    dL, dW1, db1 = affine_backward(dL2,cache1)
+
+    dW1+=self.reg*self.params['W1']
+    grads['W1'] = dW1
+    grads['b1'] = db1
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
